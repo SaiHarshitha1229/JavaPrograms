61dc0969ce3b36100958c0697d814ba34f9b575c
SaiHarshitha1229 <harshitha1229@sasi.ac.in>
2022-07-22 15:35:33 +0530

    add

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+class PrimitiveDataTypes
+{
+	public static void main(String args[])
+	{
+		byte b=3;
+		System.out.println("Byte value is :"+b);
+		int i=10;
+		System.out.println("Int value is :"+i);
+		long l=20;
+		System.out.println("Long value is :"+l);
+		short s=40;
+		System.out.println("Short value is :"+s);
+		float f=10f;
+		System.out.println("Float value is :"+f);
+		double d=29;
+		System.out.println("Double value is :"+d);
+		char c='s';
+		System.out.println("Character value is :"+c);
+	}
+}
